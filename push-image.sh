c3be3cd5a4e3cb4e0c08d1fdf082c946d6db4c4b
James Spears <jamesspearsv@gmail.com>
2025-02-09 11:24:39 -0500

    Add container image publishing script and automation

---
+++ push-image.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# DESCRIPTION:
+# Simple script to automate building and publishing the 
+# latest Writr.md container image to the Github container registry.
+# Currently this script will work in my local dev environment only! 
+# There are plans to add improved CI/CD using Github workflows and actions.
+
+# build and tag images
+echo "Starting build process..."
+docker build -t writrmd .
+docker tag writrmd ghcr.io/jamesspearsv/writrmd:latest
+
+# login to github container registry
+echo "Logging into ghcr.io..."
+echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin
+
+# push updated image to ghcr.io
+docker push ghcr.io/jamesspearsv/writrmd:latest
+echo "Successfully published image!"
