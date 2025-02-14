a9c40a9fee296248b488ceda6e161d94c3e27890
James Spears <jamesspearsv@gmail.com>
2025-02-14 13:09:37 -0500

    Add metadata to public routes
    Adds metadata to all public routes using Next.js Metadata API

--- src/app/(routes)/blog/[slug]/page.tsx
+++ src/app/(routes)/blog/[slug]/page.tsx
@@ -1,6 +1,28 @@
+import { fetchPostBySlug } from '@/src/app/lib/actions';
 import Post from '@/src/app/ui/blog/Post';
 import Breadcrumbs from '@/src/app/ui/common/Breadcrumbs';
 import ScrollBack from '@/src/app/ui/common/ScrollBack';
+import { Metadata } from 'next';
+
+type PostProps = {
+  params: Promise<{ slug: string }>;
+};
+
+export async function generateMetadata({
+  params,
+}: PostProps): Promise<Metadata> {
+  const slug = (await params).slug;
+  const post = await fetchPostBySlug(slug);
+
+  if (post) {
+    return {
+      title: post.data.title,
+      description: post.data.excerpt,
+    };
+  }
+
+  return {};
+}
 
 export default async function PostPage({
   params,
