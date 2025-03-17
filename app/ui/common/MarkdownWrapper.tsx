a22a59f313388bccb3bb3275d1c23cb011fbd89a
James Spears <jamesspearsv@gmail.com>
2025-03-16 22:03:39 -0400

    Add markdown wrapper component and starting styles

---
+++ app/ui/common/MarkdownWrapper.tsx
@@ -0,0 +1,17 @@
+// todo: remove marked-react
+// import Markdown from 'marked-react';
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import './MarkdownWrapper.css';
+
+interface MarkdownWrapperProps {
+  value: string;
+}
+
+export default function MarkdownWrapper(props: MarkdownWrapperProps) {
+  return (
+    <div className="markdown-wrapper">
+      <Markdown remarkPlugins={[remarkGfm]}>{props.value}</Markdown>
+    </div>
+  );
+}
