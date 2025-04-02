b51880f7842400a40d5a84a8a798041c6eb5ad09
James Spears <jamesspearsv@gmail.com>
2025-04-02 13:57:19 -0400

    Fixed post layout issue

--- app/ui/posts/Post.tsx
+++ app/ui/posts/Post.tsx
@@ -5,29 +5,34 @@ import Link from 'next/link';
 import MarkdownWrapper from '@/app/ui/common/MarkdownWrapper';
 
 export default async function Post(props: { slug: string }) {
-  const file = await fetchPostBySlug(props.slug);
+  const post = await fetchPostBySlug(props.slug);
 
-  //  404 if file is null
-  if (!file) notFound();
+  //  404 if post is null
+  if (!post) notFound();
 
-  const date = new Date(file.data.date);
+  const date = new Date(post.data.date);
 
   return (
     <article className={styles.container}>
       <aside className={styles.frontMatter}>
-        <div>{file.data.author}</div>
-        <div className={styles.date}>{date.toLocaleDateString()}</div>
+        <h1 className={styles.title}>{post.data.title}</h1>
+        <div className={styles.byline}>
+          <div className={styles.bylineItem}>{post.data.author}</div>
+          <div>—</div>
+          <div className={styles.bylineItem}>{date.toDateString()}</div>
+        </div>
         <div className={styles.tags}>
-          {file.data.tags &&
-            file.data.tags.map((tag) => (
+          {post.data.tags &&
+            post.data.tags.map((tag) => (
               <Link key={tag} href={`/blog?tag=${tag}`}>
                 <button className={styles.tag}>{tag}</button>
               </Link>
             ))}
         </div>
       </aside>
-      <section>
-        <MarkdownWrapper value={file.content} />
+      <hr />
+      <section className={styles.post}>
+        <MarkdownWrapper value={post.content} />
       </section>
     </article>
   );
