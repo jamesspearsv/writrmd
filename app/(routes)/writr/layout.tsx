38f7f3337a68bbc214d1601590694b438f7327f1
James Spears <jamesspearsv@gmail.com>
2025-04-09 19:46:53 -0400

    Refactor admin and site navigation components

--- app/(routes)/writr/layout.tsx
+++ app/(routes)/writr/layout.tsx
@@ -1,10 +1,5 @@
-import VNav from '@/app/ui/nav/VNav';
+import AdminNav from '@/app/ui/nav/AdminNav';
 import styles from './layout.module.css';
-import ThemePicker from '@/app/ui/themes/ThemePicker';
-import { LogOut, Layout as FeatherLayout } from 'react-feather';
-import { logout } from '@/app/lib/authActions';
-import Link from 'next/link';
-import StyledButton from '@/app/ui/common/StyledButton';
 
 export default function Layout({
   children,
@@ -13,27 +8,7 @@ export default function Layout({
 }) {
   return (
     <>
-      <VNav>
-        <hr />
-        <br />
-        <div className={styles.buttonContainer}>
-          <ThemePicker />
-          <StyledButton
-            className={styles.button}
-            onClick={async () => {
-              'use server';
-              await logout();
-            }}
-          >
-            <LogOut size={22} />
-          </StyledButton>
-          <Link href={'/'}>
-            <div className={styles.button}>
-              <FeatherLayout size={22} />
-            </div>
-          </Link>
-        </div>
-      </VNav>
+      <AdminNav />
       <main className={styles.main}>
         <section className={styles.section}>{children}</section>
       </main>
---
+++ app/ui/nav/AdminNav.tsx
@@ -0,0 +1,68 @@
+import Link from 'next/link';
+import styles from './AdminNav.module.css';
+import { roboto_slab } from '@/app/ui/fonts';
+import { BookOpen, Home, Layout, LogOut, Settings } from 'react-feather';
+import ThemePicker from '@/app/ui/themes/ThemePicker';
+import StyledButton from '@/app/ui/common/StyledButton';
+import { logout } from '@/app/lib/authActions';
+
+// todo: #54 Refactor VNav to centralize component elements
+
+export default function AdminNav() {
+  return (
+    <nav className={styles.nav}>
+      <div>
+        <h1 className={roboto_slab.className}>Writr.md</h1>
+        <ul className={styles.list}>
+          <NavItem href="/writr" label="Dashboard">
+            <Home size={16} />
+          </NavItem>
+          <NavItem href="/writr/posts" label="Posts">
+            <BookOpen size={16} />
+          </NavItem>
+          <NavItem href="/writr/settings" label="Settings">
+            <Settings size={16} />
+          </NavItem>
+        </ul>
+      </div>
+      <div className={styles.actions}>
+        <div className={styles.buttonContainer}>
+          <ThemePicker />
+          <StyledButton
+            className={styles.button}
+            onClick={async () => {
+              'use server';
+              await logout();
+            }}
+          >
+            <LogOut size={22} />
+          </StyledButton>
+          <Link href={'/'}>
+            <div className={styles.button}>
+              <Layout size={22} />
+            </div>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+// todo: #56 Improve NavItem component versatility
+function NavItem(props: {
+  href: string;
+  label: string;
+  target?: '_blank';
+  children: React.ReactNode;
+}) {
+  return (
+    <li key={props.href}>
+      <Link href={props.href} className={styles.itemLink} target={props.target}>
+        <div className={styles.item}>
+          {props.children}
+          {props.label}
+        </div>
+      </Link>
+    </li>
+  );
+}
