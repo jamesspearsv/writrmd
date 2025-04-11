196ea32c677b1b989495ecc5d5a40464cbb22af3
James Spears <jamesspearsv@gmail.com>
2025-04-10 21:42:34 -0400

    Add minimal post previews to landing page

--- app/(routes)/(site)/page.tsx
+++ app/(routes)/(site)/page.tsx
@@ -1,18 +1,50 @@
-import { readSettings } from '@/app/lib/actions';
+import { fetchAllPosts, readSettings } from '@/app/lib/actions';
 import styles from './page.module.css';
 import PlaceholderPage from '@/app/ui/common/PlaceholderPage';
 import MarkdownWrapper from '@/app/ui/common/MarkdownWrapper';
+import PostPreview from '@/app/ui/posts/PostPreview';
+import Header from '@/app/ui/common/Header';
+import { ArrowRight } from 'react-feather';
+import Link from 'next/link';
 
 export default async function Home() {
   const settings = await readSettings();
+  const posts = await fetchAllPosts();
+
+  /* todo: Add post preview limit
+   * Limit to 3 to 5 most recent posts.
+   * Consider implementing this as a param in fetchAllPosts.
+   */
+
+  // todo: Add social links
 
   if (!settings.success) return <PlaceholderPage />;
 
   return (
     <div className={styles.container}>
-      <MarkdownWrapper
-        value={`# ${settings.data.name}\n\n${settings.data.summary}`}
-      />
+      <div className={styles.hero}>
+        <Header>
+          <h1>{settings.data.name}</h1>
+        </Header>
+        <MarkdownWrapper value={`${settings.data.summary}`} />
+      </div>
+      {posts.success && (
+        <div className={styles.recent_posts}>
+          <h2>Recent Posts</h2>
+          <hr />
+          {posts.data.map((post, index) => {
+            if (post.data.published) {
+              return <PostPreview key={index} post={post} variant="minimal" />;
+            } else return null;
+          })}
+          <Link href={'/blog'} className={styles.all_posts}>
+            <div>
+              <div>All Posts</div>
+              <ArrowRight size={18} />
+            </div>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
--- app/ui/posts/PostPreview.tsx
+++ app/ui/posts/PostPreview.tsx
@@ -2,22 +2,41 @@ import { Post } from '@/app/lib/definitions';
 import styles from './PostPreview.module.css';
 import Link from 'next/link';
 
-export default function PostPreview(props: { post: Post }) {
+export default function PostPreview({
+  variant = 'full',
+  ...props
+}: {
+  post: Post;
+  variant: 'minimal' | 'full';
+}) {
   const { post } = props;
 
-  return (
-    <article className={styles.article}>
-      <div className={styles.title}>
-        <Link href={`/blog/${post.data.slug}`}>
-          <h2>{post.data.title}</h2>
-        </Link>
-      </div>
-      {post.data.excerpt && <p>{post.data.excerpt}</p>}
-      <p className={styles.byline}>
-        By {post.data.author}
-        <span className={styles.span}>--</span>
-        {new Date(post.data.date).toDateString()}
-      </p>
-    </article>
-  );
+  if (variant === 'full') {
+    return (
+      <article className={styles.full}>
+        <div className={styles.title}>
+          <Link href={`/blog/${post.data.slug}`}>
+            <h2>{post.data.title}</h2>
+          </Link>
+        </div>
+        {post.data.excerpt && <p>{post.data.excerpt}</p>}
+        <p className={styles.byline}>
+          By {post.data.author}
+          <span className={styles.span}>--</span>
+          {new Date(post.data.date).toDateString()}
+        </p>
+      </article>
+    );
+  }
+
+  if (variant === 'minimal') {
+    return (
+      <Link className={styles.post_link} href={`/blog/${post.data.slug}`}>
+        <article className={styles.minimal}>
+          <div>{post.data.title}</div>
+          <div>{new Date(post.data.date).toDateString()}</div>
+        </article>
+      </Link>
+    );
+  }
 }
